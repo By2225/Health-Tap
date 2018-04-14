3952b2593a06329213f2aa2822f1b86e9749e261
By2225 <by2225@columbia.edu>
2018-04-14 19:38:01 -0400

    Add files via upload
    - Adding all necessary files to run the web app interface

---
+++ div.js
@@ -0,0 +1,11 @@
+function myFunction1() {
+    alert("CALLING 911");
+}
+
+function myFunction2() {
+    alert("THESE ARE ALL OF SPECIAL HEALTH SERVICES NEARBY");
+}
+
+function myFunction3() {
+    alert("SENDING LOCATION AND MESSAGE TO 911");
+}
